309e9b6201b00821965848fc4c33ed9ab2caddfe
Hongjun Chen <nanjingchj@gmail.com>
2021-02-26 19:38:00 -0500

    Select image and display in UI

--- FrontEnd/iOS/FoodClassifier/FoodClassifier/ViewController.m
+++ FrontEnd/iOS/FoodClassifier/FoodClassifier/ViewController.m
@@ -18,5 +18,34 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)getImage {
+    @autoreleasepool {
+        UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        picker.delegate = self;
+        picker.allowsEditing = NO;
+        [self presentViewController:picker animated:YES completion:^{
+            NSLog(@"Finished loading image chooser");
+        }];
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+    UIImageView* view = (UIImageView*)[self.view viewWithTag:192];
+    view.image = chosenImage;
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"Image chooser closed");
+    }];
+    
+    NSLog(@"Image picked");
+}
+
+
+- (IBAction)onUploadButtonPress:(id)sender {
+    [self getImage];
+}
+
 
 @end
